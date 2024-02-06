38548dde0a27abc960903c3741184602d2fdbf12
bibhab <officialbibhab@gmail.com>
2024-02-06 09:58:25 +0545

    streak logic fixed

--- backend/commit/views.py
+++ backend/commit/views.py
@@ -8,19 +8,31 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
 from django.db.models import Count
-from datetime import timedelta
+from datetime import timedelta,date
 
 # Create your views here.
 
+def calculate_streak(querysets):
+    today=date.today()
+    current_streak = 0
+    for queryset in reversed(querysets):
+        if queryset.date == today -timedelta(days=current_streak):
+            current_streak += 1
+        else:
+            break
+    return current_streak
+
 class CommitView(APIView):
     permission_classes = [IsAuthenticated]
-
     def get(self, request):
         user = request.user
-        queryset = Commit.objects.filter(user=user)
+        queryset = Commit.objects.filter(user=user).order_by('date')
         serializer = CommitSerializer(queryset, many=True)
+        streak = calculate_streak(queryset)
         data={
             'data':serializer.data,
-            'streak':5
+            'streak':streak
         }
         return Response(data)
+
+
